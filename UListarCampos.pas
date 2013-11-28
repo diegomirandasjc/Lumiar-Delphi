e9355933675572e0fe8e7a78fdecec0373d55953
diegomirandasjc <diego_miranda@outlook.com>
2013-11-28 09:25:57 -0200

    .

--- UListarCampos.pas
+++ UListarCampos.pas
@@ -11,7 +11,7 @@ type
       FTabelas: TClientDataSet;
 
       procedure CriarClientDataSetCampos;
-      function GetSQLNomeTabelas: String; virtual; abstract;
+      function GetSQLCampos: String; virtual; abstract;
    public
       constructor Create(Conexao: TSQLConnection);
       destructor Destroy;
@@ -22,19 +22,19 @@ end;
 type
    TListarCamposSQLServer = class(TListarCampos)
    private
-      function GetSQLNomeTabelas: String; override;
+      function GetSQLCampos: String; override;
 end;
 
 type
    TListarCamposFirebird = class(TListarCampos)
    private
-      function GetSQLNomeTabelas: String; override;
+      function GetSQLCampos: String; override;
 end;
 
 type
    TListarCamposInterbase = class(TListarCampos)
    private
-      function GetSQLNomeTabelas: String; override;
+      function GetSQLCampos: String; override;
 end;
 
 implementation
@@ -53,8 +53,11 @@ begin
    FTabelas := TClientDataSet.Create(nil);
 
    FTabelas.FieldDefs.Add('Codigo', ftInteger);
-   FTabelas.FieldDefs.Add('NomeTabela', ftString, 100);
+   FTabelas.FieldDefs.Add('NomeCampo', ftString, 100);
    FTabelas.FieldDefs.Add('Descricao', ftString, 8000);
+   FTabelas.FieldDefs.Add('Tamanho', ftInteger);
+   FTabelas.FieldDefs.Add('Precisao', ftInteger);
+   FTabelas.FieldDefs.Add('Obrigatorio', ftBoolean);
 
    FTabelas.CreateDataSet;
 end;
@@ -80,7 +83,7 @@ begin
    try
       QueryAux := TSQLQuery.Create(nil);
       QueryAux.SQLConnection := FConexao;
-      QueryAux.SQL.Text := GetSQLNomeTabelas;
+      QueryAux.SQL.Text := GetSQLCampos;
       QueryAux.Open;
 
       while not QueryAux.Eof do
@@ -98,7 +101,7 @@ end;
 
 { TListarCamposSQLServer }
 
-function TListarCamposSQLServer.GetSQLNomeTabelas: String;
+function TListarCamposSQLServer.GetSQLCampos: String;
 begin
    Result := 'SELECT ID, '+ #13#10 +
              '       NAME as NOMETABELA'+ #13#10 +
@@ -108,24 +111,40 @@ end;
 
 { TListarCamposInterbase }
 
-function TListarCamposInterbase.GetSQLNomeTabelas: String;
-begin
-   Result := 'SELECT RDB$RELATION_ID as ID,' + #13#10 + 
-                '       RDB$RELATION_NAME as NOMETABELA' + #13#10 +
-                '  FROM RDB$RELATIONS' + #13#10 +
-                ' WHERE RDB$VIEW_BLR IS NULL' + #13#10 +
-                '   AND (RDB$SYSTEM_FLAG = 0 OR RDB$SYSTEM_FLAG IS NULL)';
+function TListarCamposInterbase.GetSQLCampos: String;
+begin                                                
+   Result := 'SELECT A.RDB$FIELD_NAME NOMECAMPO,' + #13#10 +
+             '       C.RDB$TYPE_NAME TIPO,' + #13#10 +
+             '       B.RDB$FIELD_LENGTH TAMANHO,' + #13#10 +
+             '       B.RDB$FIELD_PRECISION PRECISAO,' + #13#10 +
+             '       B.RDB$FIELD_SCALE CASASDECIMAIS,' + #13#10 +
+             '       A.RDB$NULL_FLAG OBRIGATORIO' + #13#10 +
+             '  FROM RDB$RELATION_FIELDS A' + #13#10 +
+             ' INNER JOIN RDB$FIELDS B' + #13#10 +
+             '    ON B.RDB$FIELD_NAME = A.RDB$FIELD_SOURCE' + #13#10 +
+             ' INNER JOIN RDB$TYPES C' + #13#10 +
+             '    ON C.RDB$TYPE = B.RDB$FIELD_TYPE' + #13#10 +
+             '   AND C.RDB$FIELD_NAME = ''RDB$FIELD_TYPE''' + #13#10 +
+             ' WHERE A.RDB$RELATION_NAME = ''CAMPOS''';
 end;
 
 { TListarCamposFirebird }
 
-function TListarCamposFirebird.GetSQLNomeTabelas: String;
+function TListarCamposFirebird.GetSQLCampos: String;
 begin
-   Result := 'SELECT RDB$RELATION_ID as ID,' + #13#10 + 
-             '       RDB$RELATION_NAME as NOMETABELA' + #13#10 +
-             '  FROM RDB$RELATIONS' + #13#10 +
-             ' WHERE RDB$VIEW_BLR IS NULL' + #13#10 +
-             '   AND (RDB$SYSTEM_FLAG = 0 OR RDB$SYSTEM_FLAG IS NULL)';
+   Result := 'SELECT A.RDB$FIELD_NAME NOMECAMPO,' + #13#10 +
+             '       C.RDB$TYPE_NAME TIPO,' + #13#10 +
+             '       B.RDB$FIELD_LENGTH TAMANHO,' + #13#10 +
+             '       B.RDB$FIELD_PRECISION PRECISAO,' + #13#10 +
+             '       B.RDB$FIELD_SCALE CASASDECIMAIS,' + #13#10 +
+             '       A.RDB$NULL_FLAG OBRIGATORIO' + #13#10 +
+             '  FROM RDB$RELATION_FIELDS A' + #13#10 +
+             ' INNER JOIN RDB$FIELDS B' + #13#10 +
+             '    ON B.RDB$FIELD_NAME = A.RDB$FIELD_SOURCE' + #13#10 +
+             ' INNER JOIN RDB$TYPES C' + #13#10 +
+             '    ON C.RDB$TYPE = B.RDB$FIELD_TYPE' + #13#10 +
+             '   AND C.RDB$FIELD_NAME = ''RDB$FIELD_TYPE''' + #13#10 +
+             ' WHERE A.RDB$RELATION_NAME = ''CAMPOS''';
 end;
 
 
